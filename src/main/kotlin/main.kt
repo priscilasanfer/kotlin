90d234fa06cd866844f58de36cbc1f6200053165
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-23 17:44:31 -0300

    Criando classes para cada funcionário

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,6 @@ fun main() {
         nome = "Malu",
         cpf = "23455655643",
         salario = 1000.00,
-        tipo = 0
     )
 
     println("Nome: ${malu.nome}")
@@ -12,6 +11,24 @@ fun main() {
     println("Salario: ${malu.salario}")
 
     println("Calcula bonificação: ${malu.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+
+    if (fran.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autentição")
+    }
 }
 
 
