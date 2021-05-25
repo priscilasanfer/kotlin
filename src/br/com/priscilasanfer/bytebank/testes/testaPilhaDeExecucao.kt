109bc09458173f75f8d43ee1aea1e6f9b795bbc1
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-25 13:41:22 -0300

    Curso 4 - Lidando com exceptions com o try expression

--- src/br/com/priscilasanfer/bytebank/testes/testaPilhaDeExecucao.kt
+++ src/br/com/priscilasanfer/bytebank/testes/testaPilhaDeExecucao.kt
@@ -1,5 +1,8 @@
 package br.com.priscilasanfer.bytebank.testes
 
+import br.com.priscilasanfer.bytebank.modelo.Endereco
+import java.lang.ClassCastException
+
 fun testaPilhaDeExecucao() {
     println("inicio main")
     funcao1()
@@ -14,8 +17,15 @@ fun funcao1() {
 
 fun funcao2() {
     println("inicio funcao 2")
-    for (i in 1..5){
+    for (i in 1..5) {
         println(i)
+        val endereco = Any()
+
+        try {
+            endereco as Endereco
+        } catch (e: ClassCastException) {
+            println("ClassCastException foi pegada")
+        }
     }
     println("fim função 2")
 }
