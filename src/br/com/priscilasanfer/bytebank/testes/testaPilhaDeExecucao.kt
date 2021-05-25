34703246e254836bbabc7d4bc398811983e8bda8
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-25 14:19:37 -0300

    Utilizando as expressões

--- src/br/com/priscilasanfer/bytebank/testes/testaPilhaDeExecucao.kt
+++ src/br/com/priscilasanfer/bytebank/testes/testaPilhaDeExecucao.kt
@@ -2,17 +2,46 @@ package br.com.priscilasanfer.bytebank.testes
 
 import br.com.priscilasanfer.bytebank.modelo.Endereco
 import java.lang.ClassCastException
+import java.lang.NumberFormatException
 
 fun testaPilhaDeExecucao() {
-    println("inicio main")
+    println("início main")
+    val entrada: String = "1.0"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+    if (valorComTaxa != null) {
+        println("valor recebido: $valorComTaxa")
+    } else {
+        println("valor inválido")
+    }
+
+
     funcao1()
     println("fim main")
 }
 
 fun funcao1() {
-    println("inicio funcao 1")
-    funcao2()
-    println("fim função 1")
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastException foi pegada")
+    }
+    println("fim funcao1")
 }
 
 fun funcao2() {
@@ -20,13 +49,7 @@ fun funcao2() {
     for (i in 1..5) {
         println(i)
         val endereco = Any()
-
-        try {
-            endereco as Endereco
-        } catch (e: ClassCastException) {
-            e.printStackTrace()
-            println("ClassCastException foi pegada")
-        }
+        endereco as Endereco
     }
     println("fim função 2")
 }
