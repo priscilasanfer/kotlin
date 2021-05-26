944eb042f8abc0cde59325e7f3503ce972bcf17e
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-25 23:43:11 -0300

    Curso 4 - Testa Elvis e Safe Cast

---
+++ src/br/com/priscilasanfer/bytebank/testes/testaElvisESafeCast.kt
@@ -0,0 +1,20 @@
+package br.com.priscilasanfer.bytebank.testes
+
+import br.com.priscilasanfer.bytebank.modelo.Endereco
+
+fun testaElvisESafeCast(){
+    var enderecoNulo: Endereco? = Endereco(logradouro = "rua vergueiro", complemento = "prédio")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let {
+        println(it.logradouro.length)
+        val tamanhoComplemento: Int =
+            it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser vazio")
+        println(tamanhoComplemento)
+    }
+    teste("")
+    teste(1)
+}
+
+fun teste(valor: Any){
+    val numero: Int? = valor as? Int
+}
