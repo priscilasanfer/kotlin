8ecf5353149c92e6cbbcce0ba566f068899a0833
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-26 17:34:03 -0300

    Curso 5 - Trabalhando com listas

---
+++ collections/src/br/com/priscilasanfer/list/ListaDeLivros.kt
@@ -0,0 +1,47 @@
+package br.com.priscilasanfer.list
+
+fun main() {
+    val livro1 = Livro(
+        titulo = "Livro 1",
+        autor = "Autor livro 1",
+        anoPublicacao = 1956
+    )
+
+    val livro2 = Livro(
+        titulo = "Livro 2",
+        autor = "Autor livro 2",
+        anoPublicacao = 1942,
+        editora = "Editora livro 2"
+    )
+
+    val livro3 = Livro(
+        titulo = "Livro 3",
+        autor = "Autor livro 3",
+        anoPublicacao = 1942,
+    )
+
+    val livro4 = Livro(
+        titulo = "Livro 4",
+        autor = "Autor livro 4",
+        anoPublicacao = 1942,
+        editora = "Editora livro 4"
+    )
+
+    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
+
+    livros.add(
+        Livro(
+            titulo = "Livro 5",
+            autor = "Autor livro 5",
+            anoPublicacao = 1999,
+            editora = "Editora livro 5"
+        )
+    )
+
+    println(livros)
+
+    livros.remove(livro1)
+
+    println(livros)
+
+}
