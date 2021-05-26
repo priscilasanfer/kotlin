4f405c2c82c641f97fdc7b7bf2179b7a35ac2156
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-26 17:41:02 -0300

    Curso 5 -  Imprimindo listas formatadas

--- collections/src/br/com/priscilasanfer/list/ListaDeLivros.kt
+++ collections/src/br/com/priscilasanfer/list/ListaDeLivros.kt
@@ -38,10 +38,19 @@ fun main() {
         )
     )
 
-    println(livros)
+   livros.imprimeComMarcadores()
 
     livros.remove(livro1)
 
-    println(livros)
+    livros.imprimeComMarcadores()
+
+}
+
+fun MutableList<Livro>.imprimeComMarcadores() {
+    val textoFormatado = this.joinToString(separator = "\n") {
+        " - ${it.titulo} de ${it.autor}"
+    }
+
+    println(" ### Lista de Livros ### \n$textoFormatado")
 
 }
