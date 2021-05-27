e70f12a581b8e36504d2b95e87f2e4f689070d9d
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-27 10:05:26 -0300

    Curso 6 - Tipos de coleção

--- README.md
+++ README.md
@@ -46,3 +46,11 @@ https://cursos.alura.com.br/formacao-kotlin
 - Utilize operações de agregação para manipular coleções
 - Compreenda a diferença entre coleções mutáveis e imutáveis
 
+### Curso 6: Kotlin Collections: Set e Map
+- Entenda o que são Collections
+- Saiba a base a partir das referências Collection e Iterable
+- Conheça o Set e entenda quando utilizá-lo
+- Crie estruturas de chave e valor com o Map
+- Utilize operações específicas de Set ou Map
+- Conheça funções relacionadas a List, Set e Map
+
--- collections/src/br/com/priscilasanfer/main.kt
+++ collections/src/br/com/priscilasanfer/main.kt
@@ -1,7 +1,38 @@
 package br.com.priscilasanfer
 
-import br.com.priscilasanfer.array.salarioComBigDecimal
-
 fun main(){
-    salarioComBigDecimal()
+
+    val banco = BancoDeNomes()
+    banco.salva("Copo")
+    println(banco.nomes)
+
+    println(BancoDeNomes().nomes)
+
+
+}
+
+class BancoDeNomes{
+    val nomes: Collection<String>get()= Companion.dados
+
+    fun salva(nome: String){
+        Companion.dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: List<String> = listOf(
+        "Meg",
+        "Malu",
+        "Rafael",
+        "Regina"
+    )
+
+    println(nomes)
+
+    println("Tem Nome Rafael? ${nomes.contains("Rafael")}")
+    println("tAMANHo da Lista: ${nomes.size}")
 }
