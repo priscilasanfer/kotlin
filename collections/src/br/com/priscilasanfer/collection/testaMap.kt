b1c08b43eb438f546e49352faa1b80df296a88af
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-27 18:59:05 -0300

    Curso 6 - Operadores Especificos, busca, leitura e filtros do Map

---
+++ collections/src/br/com/priscilasanfer/collection/testaMap.kt
@@ -0,0 +1,36 @@
+package br.com.priscilasanfer.collection
+
+fun testaMap() {
+    val pedidos = mutableMapOf<Int, Double>(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
+    println(pedidos)
+
+    val pedido = pedidos[3]
+
+    pedido?.let {
+        println("pedido ${it}")
+    }
+
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("Numero do p ${p.key}")
+        println("Valor do p ${p.value}")
+    }
+
+    pedidos[3] = 70.0
+    println(pedidos)
+    pedidos.put(4, 80.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(1, 50.0)
+    println(pedidos)
+}
