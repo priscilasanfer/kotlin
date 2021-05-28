d799fc862c6fb065bc369d6f928fc94a42543ee1
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-28 10:30:21 -0300

    Curso 7 - Funcoes  Lambdas e Anonimas  com múltiplos retornos

--- funcional/src/br/com/priscilasanfer/main.kt
+++ funcional/src/br/com/priscilasanfer/main.kt
@@ -1,10 +1,30 @@
 package br.com.priscilasanfer
 
 fun main() {
-    testaTipoFuncaoReferencia()
-    testaTipoFuncaoClasse()
-    testaFuncaoLambda()
-    testaFuncaoAnonima()
+    TestaFuncaoAnonimaComMultiplosRetornos()
+    TestaFuncaoLambdaComMultiplosRetornos()
+}
+
+fun TestaFuncaoAnonimaComMultiplosRetornos() {
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) {
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+
+    println(calculaBonificacaoAnonima(1001.0))
+}
+
+fun TestaFuncaoLambdaComMultiplosRetornos() {
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50
+        }
+        salario + 100.0
+    }
+
+    println(calculaBonificacao(1001.0))
 }
 
 fun testaFuncaoLambda() {
