40d60c1f85bf0e7b8a360b5d3a35214a45717498
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-28 13:32:42 -0300

    Curso 7 - Explorando implementações com let

--- funcional/src/br/com/priscilasanfer/main.kt
+++ funcional/src/br/com/priscilasanfer/main.kt
@@ -1,5 +1,27 @@
 package br.com.priscilasanfer
 
+import java.util.*
+
 fun main() {
+//    val endereco = Endereco(logradouro = "Rua Um", numero = 3258)
+//    val enderecoEmMaisculo = "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+//    println(enderecoEmMaisculo)
+
+    Endereco(logradouro = "Rua Um", numero = 3258)
+        .let { endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".uppercase(Locale.getDefault())
+        }
+        .let(::println)
+
+    listOf(
+        Endereco(complemento = "casa"),
+        Endereco(),
+        Endereco(complemento = "apartamento")
+    )
+        .filter { endereco ->
+            endereco.complemento.isNotEmpty()
+        }.let(::println)
+
 
 }
+
