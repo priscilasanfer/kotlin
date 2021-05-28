eb6f4c607c66cdb66fa46fdd5a8745e0abe30d3d
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-28 18:19:37 -0300

    Curso 7 -  Implementando funções com receivers

--- funcional/src/br/com/priscilasanfer/testes/testaHOF.kt
+++ funcional/src/br/com/priscilasanfer/testes/testaHOF.kt
@@ -5,18 +5,34 @@ import br.com.priscilasanfer.modelo.SistemaInterno
 
 
 fun testaHOF() {
-    somar(1, 2, resultado = (::println))
+    somar(1, 2, resultado = { valor ->
+        println(valor)
+    })
+    somaReceiver(1, 2, resultado = {
+        println(this)
+    })
 
     val autenticavel = object : Autenticavel {
         val senha = 1234
         override fun autentica(senha: Int) = this.senha == senha
     }
 
-    SistemaInterno().entra(autenticavel, 1234) {
+    val sistema = SistemaInterno()
+
+    sistema.entra(autenticavel, 1234, autenticado = {
         println("Realizar pagamento")
-    }
+    })
+    sistema.entraReceiver(autenticavel, 123, autenticado = {
+        pagamento()
+    })
+
 }
 
 fun somar(a: Int, b: Int, resultado: (Int) -> Unit) {
     resultado(a + b)
+}
+
+fun somaReceiver(a: Int, b: Int, resultado: Int.() -> Unit) {
+    val total = a + b
+    total.resultado()
 }
